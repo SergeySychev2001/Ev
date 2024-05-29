ecab7834e010bbb705653a9bc3779f12b33702f1
Sergey <s.sychev@business-digit.ru>
2024-05-29 18:23:33 +0300

    3

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,44 @@
-import * as flsFunctions from './modules/functions.js';
+import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebP();
+
+const generalSwiper = new Swiper(".general-swiper", {
+	spaceBetween: 30,
+	loop: true,
+	autoHeight: true,
+	autoplay: {
+		delay: 5000,
+	},
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		1000: {
+			slidesPerView: 2,
+		},
+	},
+});
+
+const objectsSwiper = new Swiper(".objects-swiper", {
+	autoplay: {
+		enabled: true,
+		delay: 0,
+		disableOnInteraction: true,
+	},
+	autoHeight: true,
+	loop: true,
+	slidesPerView: 3,
+	spaceBetween: 30,
+	speed: 12000,
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		500: {
+			slidesPerView: 2,
+		},
+		1000: {
+			slidesPerView: 3,
+		},
+	},
+});
