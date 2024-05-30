3daf799b582e74db0807fddfd97d395793a111e7
Sergey <s.sychev@business-digit.ru>
2024-05-30 14:33:53 +0300

    4

---
+++ src/js/modules/scroll.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal(".scroll-tr", {
+	distance: "30px",
+	origin: "bottom",
+	opacity: 0,
+	duration: 1000
+});
---
+++ src/js/modules/swiper.js
@@ -0,0 +1,65 @@
+const setSwiper = ({ id, delay, breakpoints }) => {
+	const el = document.querySelector(id);
+	return new Swiper(id, {
+		autoHeight: true,
+		loop: true,
+		spaceBetween: 30,
+		autoplay: {
+			enabled: Boolean(delay),
+			delay,
+		},
+		breakpoints,
+		navigation: {
+			prevEl: el.querySelector("#nav-prev"),
+			nextEl: el.querySelector("#nav-next"),
+		},
+		pagination: {
+			el: el.querySelector("#nav-paging"),
+		},
+	});
+};
+
+setSwiper({
+	id: "#general-swiper",
+	delay: 0,
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		1000: {
+			slidesPerView: 2,
+		},
+	},
+});
+
+setSwiper({
+	id: "#objects-swiper",
+	delay: 3000,
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		500: {
+			slidesPerView: 2,
+		},
+		1000: {
+			slidesPerView: 3,
+		},
+	},
+});
+
+setSwiper({
+	id: "#reviews-swiper",
+	delay: 5000,
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		500: {
+			slidesPerView: 2,
+		},
+		1000: {
+			slidesPerView: 3,
+		},
+	},
+});
